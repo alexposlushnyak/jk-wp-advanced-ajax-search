c9b9bd235fa91163ed91ba248cb8ee1aa08e423e
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-09 04:49:06 +0200

    Added ajax handler for live search (tmp)

--- jk-wp-advanced-ajax-search.php
+++ jk-wp-advanced-ajax-search.php
@@ -3,10 +3,52 @@
 class jk_wp_advanced_ajax_search
 {
 
-    public function init()
+    public function ajax_handler()
     {
 
+        wp_reset_postdata();
 
+        $args = array(
+            's' => $_POST['query'],
+            'ignore_sticky_posts' => true,
+            'posts_per_page' => 5,
+            'order' => 'DESC',
+            'post_type' => 'post',
+            'orderby' => 'rand'
+        );
+
+        $the_query = new WP_Query($args);
+
+        if ($the_query->have_posts()) :
+
+            while ($the_query->have_posts()) : $the_query->the_post();
+
+
+
+            endwhile;
+
+        else: ?>
+
+            <li class="empty-message">
+
+                <?php echo esc_html__('We did not find any article that matches this search. Try using other search criteria...', 'jk-core'); ?>
+
+            </li>
+
+        <?php
+
+        endif;
+
+        die();
+
+    }
+
+    public function ajax_init()
+    {
+
+        add_action('wp_ajax_nopriv_jk_advanced_ajax_search', [$this, 'ajax_handler']);
+
+        add_action('wp_ajax_jk_advanced_ajax_search', [$this, 'ajax_handler']);
 
     }
 
